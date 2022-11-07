6f8c93f87573120076ce5b5b1fec7bf829d7ac9c
Patrick <patrick.exner1@web.de>
2022-11-07 18:24:23 +0100

    Remove Godot License from project and fix naming bug

--- extension/src/register_types.h
+++ extension/src/register_types.h
@@ -1,7 +1,7 @@
 #ifndef SUMMATOR_REGISTER_TYPES_H
 #define SUMMATOR_REGISTER_TYPES_H
 
-void register_summator_types();
-void unregister_summator_types();
+void initialize_summator_types();
+void uninitialize_summator_types();
 
 #endif // SUMMATOR_REGISTER_TYPES_H
