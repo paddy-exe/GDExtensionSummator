f114c0f8c509bddd753bf8b12d5fe64d195286fa
Patrick <patrick.exner1@web.de>
2022-12-19 22:37:13 +0100

    Update to Beta 9

--- extension/src/register_types.cpp
+++ extension/src/register_types.cpp
@@ -1,6 +1,6 @@
 #include "register_types.h"
 #include "summator.h"
-#include <godot/gdnative_interface.h>
+#include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
@@ -26,7 +26,7 @@ extern "C"
 
 	// Initialization.
 
-	GDNativeBool GDN_EXPORT summator_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT summator_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
