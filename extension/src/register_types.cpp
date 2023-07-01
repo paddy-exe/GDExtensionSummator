bdedf2fdf9361c8da0b436d5bc7b4869629ce708
Patrick <patrick.exner1@web.de>
2023-07-01 13:03:23 +0200

    4.1 RC2 Update
    * prepares the project for the coming 4.1 release

--- extension/src/register_types.cpp
+++ extension/src/register_types.cpp
@@ -26,9 +26,8 @@ extern "C"
 
 	// Initialization.
 
-	GDExtensionBool GDE_EXPORT summator_library_init(const GDExtensionInterface *p_interface, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
-	{
-		GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+	GDExtensionBool GDE_EXPORT summator_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
 		init_obj.register_initializer(initialize_summator_types);
 		init_obj.register_terminator(uninitialize_summator_types);
